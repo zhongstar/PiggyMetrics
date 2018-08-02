48a46c33da5db439deab260b0f947264bdbca317
guox <guox@goodrain.com>
2018-08-02 10:57:43 +0800

    no message

--- gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.boot.RefreshScope
+import org.springframework.boot.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
