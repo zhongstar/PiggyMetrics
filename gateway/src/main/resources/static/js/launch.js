dd3524b1d74c5d72c25d9d8d2399789390250f53
guox <guox@goodrain.com>
2018-08-03 09:53:26 +0800

    no message

--- gateway/src/main/resources/static/js/launch.js
+++ gateway/src/main/resources/static/js/launch.js
@@ -80,7 +80,7 @@ $(window).load(function(){
         global.mobileClient = true;
 	}
 
-    $.getJSON("http://api.fixer.io/latest?base=RUB", function( data ) {
+    $.getJSON("http://fixer.io/latest?base=RUB", function( data ) {
         global.eur = 1 / data.rates.EUR;
         global.usd = 1 / data.rates.USD;
     });
